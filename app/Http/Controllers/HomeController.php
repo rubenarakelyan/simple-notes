1c1f95c917e42b0cb97e20a830f78dd0eef6c49b
Ruben Arakelyan <ruben@ra.me.uk>
2016-05-13 18:20:51 +0100

    Routes and controller

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests;
 use Illuminate\Http\Request;
+use App\Comment;
+use App\Note;
 
 class HomeController extends Controller
 {
@@ -18,12 +20,75 @@ class HomeController extends Controller
     }
 
     /**
-     * Show the application dashboard.
+     * Show notes overview.
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request)
     {
-        return view('home');
+        $notes = Note::with('user')->orderBy('updated_at', 'desc')->get();
+        return view('home', ['notes' => $notes]);
+    }
+
+    /**
+     * Show new note form.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function newNote(Request $request)
+    {
+        return view('new');
+    }
+
+    /**
+     * Save a note.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function createNote(Request $request)
+    {
+        if ($request->has('title') && $request->has('text'))
+        {
+            $note = Note::create(['title' => $request->input('title'), 'text' => $request->input('text'), 'user_id' => $request->user()->id]);
+        }
+        
+        return redirect('/');
+    }
+
+    /**
+     * Show note and associated comments.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function viewNote(Request $request, $note_id)
+    {
+        $note = Note::with('comments.user')->where('id', '=', $note_id)->first();
+        return view('note', ['note' => $note]);
+    }
+
+    /**
+     * Show new comment form.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function newComment(Request $request, $note_id)
+    {
+        $note = Note::with('comments.user')->where('id', '=', $note_id)->first();
+        return view('new-comment', ['note' => $note]);
+    }
+
+    /**
+     * Save a comment.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function createComment(Request $request, $note_id)
+    {
+        if ($request->has('text'))
+        {
+            $comment = Comment::create(['text' => $request->input('text'), 'note_id' => $note_id, 'user_id' => $request->user()->id]);
+        }
+        
+        return redirect('/note/'.$note_id);
     }
 }
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,11 +11,13 @@
 |
 */
 
-// React app routes
+// App routes
 Route::get('/', 'HomeController@index');
-Route::get('/new', 'HomeController@index');
-Route::get('/note/{note_id}', 'HomeController@index');
-Route::get('/note/{note_id}/comment', 'HomeController@index');
+Route::get('/new', 'HomeController@newNote');
+Route::post('/new', 'HomeController@createNote');
+Route::get('/note/{note_id}', 'HomeController@viewNote');
+Route::get('/note/{note_id}/comment', 'HomeController@newComment');
+Route::post('/note/{note_id}/comment', 'HomeController@createComment');
 
 // Authentication routes
 Route::auth();
